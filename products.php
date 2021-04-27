55e0bb00fd3d8280d927d4623777f205eadca060
gauravdaware <daware.gaurav@gmail.com>
2021-04-27 13:58:04 +0530

    pagination removed

--- products.php
+++ products.php
@@ -89,12 +89,12 @@ session_start();
 						}
 						?>
 						
-						<ul class="pagination">
+						<!--<ul class="pagination">
 							<li class="active"><a href="">1</a></li>
 							<li><a href="">2</a></li>
 							<li><a href="">3</a></li>
 							<li><a href="">&raquo;</a></li>
-						</ul>
+						</ul>-->
 					</div><!--features_items-->
 				</div>
 			</div>
